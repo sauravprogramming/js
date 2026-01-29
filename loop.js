874219e5dcdbad1f2481d764f906a5be3e2a967e
sauravprogramming <sauravmondolhere@gmail.com>
2026-01-29 22:26:48 +0530

    Js practice

---
+++ loop.js
@@ -0,0 +1,34 @@
+// const : value with not change
+// let : value can change
+// var : we should not use it
+
+// variable
+// const amount = 200;
+
+// if(amount > 100){
+//     console.log(amount);
+// }
+// else{
+
+// }
+
+// array
+// const numbers = [12, 35, 21, 44, 53];
+
+// console.log(numbers);
+
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ["apple", "banana", "Guava", "orange", "jackfruit"];
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat');
+
+}
+
+const num = 0;
+while (num < 10){
+    console.log('looping', num);
+}
---
+++ while.js
@@ -0,0 +1,14 @@
+const number = 50;
+if (number < 10) {
+    console.log('choto ekta number paisi')
+}
+else {
+    console.log('something new is cooking')
+}
+
+    console.log(number);
+
+    const num = 0;
+    while (num < 5){
+        console.log('loooping', num);
+    }
