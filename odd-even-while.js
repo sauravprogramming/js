65ab45ff19eb504293ba7db81c78345e9d2b4022
sauravprogramming <sauravmondolhere@gmail.com>
2026-01-30 09:35:51 +0530

    loop taks

---
+++ odd-even-while.js
@@ -0,0 +1,18 @@
+// Find all the odd numbers from 61 to 100.
+
+// let i = 61;
+
+// while( i<=100){
+//     console.log(i);
+//     i = i + 2;
+// }
+
+
+// Find all the even numbers from 78 to 98.
+
+let i = 78;
+while(i<=98){
+    console.log(i);
+    i = i + 2;
+
+}
